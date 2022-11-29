feb483c8afcce5f0d4fd171ff0eaa69b7c39a3f9
Jonathan Lacoste <joe.dwwm2022@gmail.com>
2022-11-29 16:12:49 +0100

    commit update formulaire

--- mail.php
+++ mail.php
@@ -1,5 +1,15 @@
-<?php
-$retour = mail('lacoste_jonathan@orange.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
-if ($retour)
-     echo '<p>Votre message a bien été envoyé.</p>';
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
+    <?php
+    $retour = mail('lacoste_jonathan@orange.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+    ?>
+</body>
+</html>
