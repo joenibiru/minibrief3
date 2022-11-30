725df918883eedbc38c89db4f4b35695e8455be8
Jonathan Lacoste <joe.dwwm2022@gmail.com>
2022-11-30 09:29:19 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Envoi d'un message par formulaire</title>
-</head>
-
-<body>
-    <?php
+<?php
     $retour = mail('lacoste_jonathan@orange.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
     if ($retour)
         echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
-</body>
-</html>
+?>
