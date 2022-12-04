32f48d02b48cb50d47c4d2b2d5356e7871a96742
Jonathan Lacoste <joe.dwwm2022@gmail.com>
2022-12-04 20:31:29 +0100

    update poertfolio

--- mail.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $retour = mail('lacoste_jonathan@orange.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr');
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-?>
